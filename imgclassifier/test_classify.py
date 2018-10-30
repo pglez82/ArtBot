5a439878b07d594049a4cdf8dd1e95a50e00f754
Shaila <shailaperezfernandez@gmail.com>
2018-10-30 19:03:35 +0100

    Image Classify

--- imgclassifier/test_classify.py
+++ imgclassifier/test_classify.py
@@ -1,5 +1,5 @@
 from img_classifier import PictureClassifier
 c = PictureClassifier()
-#a=c.predict('images/carreno/carreno.jpg')
-a=c.predict('C:\Users\Shaila\Desktop\Proyecto\MAbot\Escultura\F357.jpg')
+a=c.predict('images/carreno/carreno.jpg')
+
 print (a)
