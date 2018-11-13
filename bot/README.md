917a4f1ce23f0176e37d548e46df4efcf623505c
Silvia <silviaoposita@gmail.com>
2018-11-13 01:09:33 +0100

    Envio y recepcion de imagenes y documentos. Envio de broadcast a todos los usuarios. Registro de todos los usuarios y deteccion de usuario nuevo. Probando solucion problema ReadTimeOut. Implementados entrenamientos con corpus personalizados y exportacion de aprendizaje.

--- bot/README.md
+++ bot/README.md
@@ -12,6 +12,8 @@
  
  	$ pip install chatterbot
 
+	* https://github.com/eternnoir/pyTelegramBotAPI.git
+
  4º Crear el bot en bot_terminal.py, y entrenarlo añadiendole un trainer para que pueda empezar a conversar.
 
  5º Ejecutar el archivo bot_terminal.py desde la terminal y conversar con el bot
@@ -50,8 +52,6 @@
  
 	$ sudo pip install pyTelegramBotAPI --upgrade
 
-	* https://github.com/eternnoir/pyTelegramBotAPI.git
-
  2º Crear un nuevo documento (bot_telegram.py)
 
  3º Crear un objeto bot pasandole el token como parametro
@@ -68,21 +68,15 @@
     
 ### ENLAZAR CON EL CHATTERBOT
 
- 1   Instalar Chatterbot:
- 
-	$ sudo pip install chatterbot
-	
-	* https://github.com/gunthercox/ChatterBot.git 
-
- 2 En bot_telegram.py creo una funcion para enviar una peticion al chatterbot. 
+ 1 En bot_telegram.py creo una funcion para enviar una peticion al chatterbot. 
  Para ello lo creo igual que en bot_terminal.py, pero sin establecer la entrada y 
  salida de datos por el terminal y entrenandole solo en la primera ejecucion del script.
 
- 3 Paso el mensaje del usuario a la funcion y obtengo la respuesta del chatterbot que guardo en una variable
+ 2 Paso el mensaje del usuario a la funcion y obtengo la respuesta del chatterbot que guardo en una variable
 
- 4 Creo un archivo de texto para guardar la respuesta temporalmente
+ 3 Creo un archivo de texto para guardar la respuesta temporalmente
 
- 5 Leo el archivo con la respuesta y se la paso al bot de telegram para que la devuelva
+ 4 Leo el archivo con la respuesta y se la paso al bot de telegram para que la devuelva
 
  *Cuidado con cualquier simbolo raro (caracteres especiales, tildes..). En terminal no hay problema, pero en telegram rompe cuando el bot los tiene que enviar como respuesta.
 	
--- bot/bot_telegram/bot_telegram.py
+++ bot/bot_telegram/bot_telegram.py
@@ -1,41 +1,168 @@
+import os
+from os.path import exists
+import time
 import logging
-import telebot 
+import telebot
 from chatterbot import ChatBot
 
-# LINK THE BOT USING THE TOKEN
-telegram_bot = telebot.TeleBot("TOKEN")
+
+# LOAD USERS LIST
+users = list()
+for line in open('users.txt','r'):
+    id = int(line)
+    users.append(id)
 
 
-logging.basicConfig(level=logging.INFO)
-
-# SET FUNCTIONS TO RESPONSE TO THE COMMANDS 
-
-@telegram_bot.message_handler(commands=['start'])
-def start_response(message):
-	telegram_bot.reply_to(message, "Welcome to the Anton Museum")
-
-@telegram_bot.message_handler(commands=['help'])
-def help_response(message):
-	telegram_bot.reply_to(message, "I am here to help you")
-
-@telegram_bot.message_handler(commands=['web'])
-def web_response(message):
-	telegram_bot.reply_to(message, "http://museoanton.com/")
+# READ THE TOKEN
+token = ''
+if exists('token.txt'):
+    with open('token.txt') as file:
+        token = file.readline().strip()
+    print('Token loaded')
+else:
+    print('No token file detected')
 
 
-# NEW FUNCTION TO CREATE THE CHATTERBOT
-def ask_chatterbot(message):
-    chatbot = ChatBot("MuseoAntonBot",
+# LINK THE BOT WITH TELEGRAM USING THE TOKEN
+telegram_bot = telebot.TeleBot(token)
+
+logger = telebot.logger
+logging.basicConfig(level=logging.ERROR)
+
+
+# CREATE THE CHATBOT
+chatbot = ChatBot("MuseoAntonBot",
         # STORAGE
         storage_adapter = "chatterbot.storage.SQLStorageAdapter",
         database = "bot_telegram.sqlite3",
+        # LOGIC
+	    logic_adapters=[
+            "chatterbot.logic.BestMatch",
+	        {
+            'import_path': 'chatterbot.logic.LowConfidenceAdapter',
+            'threshold': 0.65,
+            'default_response': 'I am sorry, but I do not understand.'
+            }
+        ],
+        # INPUT
+        preprocessors=[
+            'chatterbot.preprocessors.clean_whitespace',
+            'chatterbot.preprocessors.convert_to_ascii'
+        ],
         # LEARNING
         trainer = "chatterbot.trainers.ChatterBotCorpusTrainer"
     )
 
-    #(ONLY THE FIRST TIME)
-    # chatbot.train("chatterbot.corpus.english")
+# CHATBOT TRAINING
+# FIND OUT WHY DOES NOT LOAD THE WHOLE FOLDER
+#chatbot.train(
+#    "chatterbot.corpus.english",
+#    "chatterbot.corpus.custom"
+#    )
 
+# EXPORT THE TRAINING
+# chatbot.trainer.export_for_training('./trained.yml')
+
+
+# BROADCAST
+def broadcast(message_to_send):
+    for user in users:
+        telegram_bot.send_message(user, message_to_send)
+
+
+# LISTENERS FOR COMMANDS
+@telegram_bot.message_handler(commands=['start'])
+def start_response(message):
+    if message.chat.id not in users:
+        telegram_bot.send_message(message.chat.id, "Welcome to the Anton Museum <b>" +
+        message.from_user.first_name + "</b>!", parse_mode="HTML")
+        users.append(message.chat.id)
+        with open('users.txt','a') as f:
+            f.write(str(message.chat.id)+'\n')
+    else:
+	   telegram_bot.send_message(message.chat.id, "Welcome back to the Anton Museum <b>" +
+       message.from_user.first_name + "</b>!", parse_mode="HTML")
+
+@telegram_bot.message_handler(commands=['help'])
+def help_response(message):
+	telegram_bot.send_message(message.chat.id, "I am here to help you")
+
+@telegram_bot.message_handler(commands=['web'])
+def web_response(message):
+	telegram_bot.send_message(message.chat.id, "<a href='http://museoanton.com/'>Click here</a>", parse_mode="HTML")
+
+# WORK IN PROGRESS
+#@telegram_bot.message_handler(commands=['logo'])
+#def web_response(message):
+#    image = 'profile_pictures/BotPic.jpeg'
+#    if exists(image):
+        
+
+
+
+# LISTENER FOR IMAGES
+# AS A DOCUMENT
+@telegram_bot.message_handler(func=lambda message: message.document.mime_type.startswith('image/'), content_types=['document'])
+def handle_image_as_document(message):
+    telegram_bot.send_message(message.chat.id, "I received your image as a document")
+    resend_image(message)
+
+# AS A PHOTO
+@telegram_bot.message_handler(content_types=['photo'])
+def handle_image_as_photo(message):
+    telegram_bot.send_message(message.chat.id, "I received your image as a photo")
+    resend_image(message)
+
+# SAVE THE IMAGE AND SEND IT BACK
+def resend_image(message):
+    # Search the biggest photo (avoid thumbnails)
+    image_file_id = None
+    base_size = 0
+    for photo in message.photo:
+        total_size = photo.height * photo.width
+        if total_size > base_size:
+            base_size = total_size
+            image_file_id = photo.file_id
+
+    # Download and write the file
+    file_info = telegram_bot.get_file(image_file_id)
+    file_download = telegram_bot.download_file(file_info.file_path)
+
+    # file_download is something like this:
+    # https://api.telegram.org/file/bot<token>/<file_path>
+
+    with open(image_file_id, 'wb') as file:
+        file.write(file_download)
+
+    # Send "uploading photo" action since can take a few seconds
+    telegram_bot.send_chat_action(message.chat.id, 'upload_photo')
+
+    # Upload the photo and do it as a reply
+    telegram_bot.send_photo(message.chat.id,
+                   open(image_file_id, 'rb'),
+                   caption= "Your image")
+    try:
+        os.unlink(image_file_id)
+    except Exception as err:
+        logger.error(err)
+
+
+# GENERIC LISTENER
+@telegram_bot.message_handler(func=lambda message:True)
+def answer_telegram(message):
+    if message.content_type == 'text':
+        if message.text == 'Broadcast':
+            broadcast('Broadcaaaaaast!')
+        else:
+            telegram_bot.send_chat_action(message.chat.id, 'typing')
+            ask_chatterbot(message.text)
+            response = open("response.txt", "r")
+            response = response.read()
+            telegram_bot.send_message(message.chat.id, response)
+
+
+# ASK TO THE CHATTERBOT
+def ask_chatterbot(message):
     response = chatbot.get_response(message)
     response = str(response)
 
@@ -44,26 +171,15 @@ def ask_chatterbot(message):
     resp_file.close()
 
 
-# SEND THE RESPONSE TO TELEGRAM
-@telegram_bot.message_handler(func=lambda message:True)
-def answer_telegram(message):
-    ask_chatterbot(message.text)
-
-    response = open("response.txt", "r")
-    response = response.read()
-
-    telegram_bot.reply_to(message, response)
-
 #Send Sculptor main work
-@telegram_bot.message_handler(func=lambda message:True)
-def send_image(self, message):
-	response = 'ruta'
-	for palabra in listaR:
-		if message.find(palabra, beg=0, end=len(string)):
-		    if os.path.exists(file_image):
-				telegram_bot.reply_to(message,content_type='photo', response)		        
-listaR = ['favorite', 'important','principal'] 
-
+#@telegram_bot.message_handler(func=lambda message:True)
+#def send_image(self, message):
+#	response = 'ruta'
+#	for palabra in listaR:
+#		if message.find(palabra, beg=0, end=len(string)):
+#		    if os.path.exists(file_image):
+#				telegram_bot.reply_to(message,content_type='photo', response)
+#listaR = ['favorite', 'important','principal']
 
 
 # KEEP THE BOT LISTENING TO TRY THE BOT NOT STOPPING
@@ -72,8 +188,4 @@ while True:
         telegram_bot.polling(none_stop=True)
     except Exception as err:
         logger.error(err)
-        time.sleep(5)
-        
-
-
-
+        time.sleep(10)
