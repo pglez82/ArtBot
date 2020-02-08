fc44c38acb832ab7badc110fd6c1d3190cc2db08
root <pglez82@gmail.com>
2020-02-08 12:13:51 +0000

    modified for backwards compatibility with mxnet

--- imgclassifier/finetuneCNN.sh
+++ imgclassifier/finetuneCNN.sh
@@ -4,9 +4,9 @@ nexamples=`wc -l < artbot_train.lst`
 #Parameters
 model=resnet-18
 finetuned_model=resnet-18-ft
-batch_size=32
+batch_size=45
 epochs=10
 
 mkdir -p models/finetuned/$finetuned_model
 
-python ~/incubator-mxnet/example/image-classification/fine-tune.py --pretrained-model models/$model/$model --data-train artbot_train.rec --data-val artbot_val.rec --load-epoch 0 --random-crop 0 --random-mirror 0 --num-epochs $epochs --rgb-mean 0,0,0 --num-classes $nclasses --model-prefix models/finetuned/$finetuned_model/$finetuned_model --batch-size $batch_size --num-examples $nexamples --layer-before-fullc 'flatten0'
+python ~/incubator-mxnet/example/image-classification/fine-tune.py --gpus 0 --pretrained-model models/$model/$model --data-train artbot_train.rec --data-val artbot_val.rec --load-epoch 0 --random-crop 0 --random-mirror 0 --num-epochs $epochs --rgb-mean 0,0,0 --num-classes $nclasses --model-prefix models/finetuned/$finetuned_model/$finetuned_model --batch-size $batch_size --num-examples $nexamples --layer-before-fullc 'flatten0'
--- imgclassifier/pack_images.sh
+++ imgclassifier/pack_images.sh
@@ -1,5 +1,5 @@
-python ~/incubator-mxnet/tools/im2rec.py artbot images_aug --list --recursive --train-ratio .8 --exts .jpg > labels.txt
-python ~/incubator-mxnet/tools/im2rec.py artbot_train.lst images_aug --pass-through --num-thread 2
-python ~/incubator-mxnet/tools/im2rec.py artbot_val.lst images_aug --pass-through --num-thread 2
+python ~/incubator-mxnet/tools/im2rec.py artbot images_aug --list TRUE --recursive TRUE --train-ratio .8 --exts .jpg > labels.txt
+python ~/incubator-mxnet/tools/im2rec.py artbot_train.lst images_aug --pass-through TRUE --num-thread 2
+python ~/incubator-mxnet/tools/im2rec.py artbot_val.lst images_aug --pass-through TRUE --num-thread 2
 
 
